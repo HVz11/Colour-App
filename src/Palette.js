bb3cbad2d98a1a9c398d60c996461cfb2ff55fde
Vaibhav <vs630916@gmail.com>
2023-01-03 12:24:12 +0530

    Added palette

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar  */}
+        <div className="Palette-colors">{/* bunch of colour boxes */}</div>
+        {/* footer */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
