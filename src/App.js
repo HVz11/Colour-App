80ca54ebdc95fdfefba5e1a65e8cf4e353b961da
Vaibhav <vs630916@gmail.com>
2023-04-13 19:57:03 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, { Component } from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Palette {...seedColors[2]} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Palette {...seedColors[4]} />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function Main() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+      root.render(<App />);
