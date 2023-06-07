184ed492ba101d7f4c5aaccaf6c4c8cd8129692c
Vaibhav <vs630916@gmail.com>
2023-06-07 19:44:29 +0530

    NewPaletteForm has been added

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,29 +1,34 @@
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
-    root: {
-      backgroundColor: "blue",
-      height: "100vh",
-      display: "flex",
-      alignItems: "flex-start",
-      justifyContent: "center"
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "white",
+    "& a": {
+      color: "white",
     },
-    container: {
-      width: "50%",
-      display: "flex",
-      alignItems: "flex-start",
-      flexDirection: "column",
-      flexWrap: "wrap"
-    },
-    nav: {
-      display: "flex",
-      width: "100%",
-      justifyContent: "space-between"
-    },
-    palettes: {
-      boxSizing: "border-box",
-      width: "100%",
-      display: "grid",
-      gridTemplateColumns: "repeat(3, 30%)",
-      gridGap: "5%"
-    }
-  };
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%",
+  },
+};
