a278dcbaf2fbad9466e8591101a6e1cd616405c4
Vaibhav <vs630916@gmail.com>
2023-06-14 18:58:44 +0530

    M

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-// import { createRoot } from "react-dom/client";
-
-ReactDOM.render(
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
